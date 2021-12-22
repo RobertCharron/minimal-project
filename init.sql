047c2cf1647e4c8df80800e1c1c702adfaa6edb8
Robert Charron <robert@clearcommedia.com>
2021-12-22 13:52:23 -0500

    Docker compose

---
+++ init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS minimal;
+
+-- CreateTable
+CREATE TABLE IF NOT EXISTS `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(180) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
